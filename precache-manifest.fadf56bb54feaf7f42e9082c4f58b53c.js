cb2c82ed23ebf3dcdbd089a7a18efd1fdfe1c74d
Mounir <mounir@MacBook-Pro-de-Mounir.local>
2020-05-24 00:45:02 +0200

    resume

---
+++ precache-manifest.fadf56bb54feaf7f42e9082c4f58b53c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0436f53896a98b7b52fb5b0c267fca49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d944b3cb9c721c37c6a",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cb70a67cffec224e160b",
+    "url": "/static/js/2.de8053c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.de8053c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d944b3cb9c721c37c6a",
+    "url": "/static/js/main.b141de3a.chunk.js"
+  },
+  {
+    "revision": "824a87c525fc41ffa25d",
+    "url": "/static/js/runtime-main.078ffda6.js"
+  }
+]);
